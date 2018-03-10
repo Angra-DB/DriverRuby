8e4639293d62b7249a4232a3bfbfae226a464357
Unknown <teofilojadiel@gmail.com>
2018-03-10 18:45:24 -0300

    Add test for the tcp connections that happens uppon the creation of the driver entity

--- angradb/spec/angradb_spec.rb
+++ angradb/spec/angradb_spec.rb
@@ -1,9 +1,13 @@
+require 'spec_helper'
+require 'byebug'
 RSpec.describe Angradb do
   it "has a version number" do
     expect(Angradb::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "should successfully open a tcp connection with Angradb" do
+    ip_address = "127.0.0.1"
+    ip_port = 1234
+    expect{Angradb::Driver.new(ip_address, ip_port)}.not_to raise_error
   end
 end
