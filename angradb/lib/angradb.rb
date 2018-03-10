3aca9f06ac793d41f13f664897b906da38d29609
Unknown <teofilojadiel@gmail.com>
2018-03-10 18:45:32 -0300

    Add implementation of the Driver constructor, it sets the ip address and port besides setting the connection

--- angradb/lib/angradb.rb
+++ angradb/lib/angradb.rb
@@ -1,5 +1,27 @@
 require "angradb/version"
+require "socket"
 
 module Angradb
-  # Your code goes here...
+  class Driver
+
+    @ip_address = nil
+    @ip_port = nil
+    @session = nil
+
+    def initialize(ip_address, ip_port)
+      @ip_address = ip_address
+      @ip_port = ip_port
+      open_tcp_connection
+    end
+
+    private
+
+    def open_tcp_connection
+      begin
+        @session = TCPSocket.new @ip_address, @ip_port
+      rescue
+        raise "Couldnt connect with the socket-server"
+      end
+    end
+  end
 end
