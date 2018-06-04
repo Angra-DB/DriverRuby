830713cea446d0eb1311b6eb7feb61a97268b366
Unknown <teofilojadiel@gmail.com>
2018-06-04 08:47:53 -0300

    Refactor the save method to only raise error on connection problems

--- angradb/lib/angradb.rb
+++ angradb/lib/angradb.rb
@@ -44,9 +44,12 @@ module Angradb
     # +key+:: returns the key for the saved document
     def save(doc)
       request = 'save ' + doc
-      response = send_to_server request
-      # check if the response is the 25 char key
-      raise 'Error on saving the document' unless response.is_a? String and response.size == 25
+      begin
+        response = send_to_server request
+      rescue
+        raise 'Error on saving the document: ' + response.to_s
+      end
+
       # returns the key without the quotes
       response.delete '"'
     end
